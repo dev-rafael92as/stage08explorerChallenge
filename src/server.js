8b6af7741fe6681debb4a6f5550b48fb0c726442
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-08-10 17:34:43 -0300

    [Stage08Challenge] Start userController + routes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.post("/users", (request, response) => {
-    const { name, email, password } = request.body;
-
-    response.send(`
-    Meu nome é ${name} meu email é ${email} e sua senha é ${password}`)
-})
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log('Server is running on Port ' + PORT))
