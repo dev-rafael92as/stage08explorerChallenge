961140fb01e61df5b246aa88da6c3ee7fc78580e
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-08-12 13:48:57 -0300

    [Stage08Challange] Add update verify password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const sqliteConection = require("../database/sqlite");
 const AppError = require("../utils/AppError");
 
@@ -22,7 +22,7 @@ class UsersController {
     }
 
     async update(request, response) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConection();
@@ -38,16 +38,30 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso.");
         }
 
-        user.name = name;
-        user.email = email;
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if(password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, password);
+
+            if(!checkOldPassword) {
+                throw new AppError("A senha antiga não confere");
+            }
+            user.password = await hash(password, 8);
+        }
 
         await database.run(`
             UPDATE users SET
             name = ?,
             email = ?,
-            updated_at = ?
+            password = ?,
+            updated_at = DATETIME('now')
             WHERE id = ?
-        `,[user.name, user.email, new Date(), id]);
+        `,[user.name, user.email, user.password, id]);
 
         return response.json();
     }
